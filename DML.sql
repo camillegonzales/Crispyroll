5ea929bbe7b4ed6cc6d9a2ea9ec690d7dff00ef6
camillegonzales <gonzcami@oregonstate.edu>
2023-11-16 13:15:35 -0800

    changed for step 4 draft

--- DML.sql
+++ DML.sql
@@ -1,7 +1,7 @@
 -- Project Title: Crispyroll
 -- Group Name: Query Queens
 -- Group 50: Camille Gonzales, Jennifer Trainor
--- CS340: Project Step 3 Draft
+-- CS340: Project Step 4 Draft
 
 -- DATA MANIPULATION QUERIES: SELECT, INSERT, UPDATE and DELETE queries
 
